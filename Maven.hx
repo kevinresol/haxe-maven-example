dc73beaec145fe04e057c0da24dcf960eaadc694
Kevin Leung <kevinresol@gmail.com>
2021-03-25 17:43:28 +0800

    Fix for windows

--- Maven.hx
+++ Maven.hx
@@ -6,12 +6,13 @@ using StringTools;
 class Maven {
 	public static function main() {
 		final out = 'maven.hxml';
+		final sep = Sys.systemName() == 'Windows' ? ';' : ':';
 		
 		// https://maven.apache.org/plugins/maven-dependency-plugin/build-classpath-mojo.html
-		final proc = new Process('mvn', ['dependency:build-classpath', '-Dmdep.outputFile=$out', '-Dmdep.pathSeparator=:']);
+		final proc = new Process('mvn', ['dependency:build-classpath', '-Dmdep.outputFile=$out', '-Dmdep.pathSeparator=$sep']);
 		switch proc.exitCode() {
 			case 0:
-				final content = File.getContent(out).split(':').map(path -> '--java-lib $path').join('\n');
+				final content = File.getContent(out).split(sep).map(path -> '--java-lib $path').join('\n');
 				File.saveContent(out, content);
 			case code:
 				Sys.println(proc.stdout.readAll().toString());
